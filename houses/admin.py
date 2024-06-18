36907525285c205dbad96d12ac919008e48763c6
mijinB <bmj13465@gmail.com>
2024-06-18 23:14:28 +0900

    django 공식 문서 보면서 사용해보기

--- houses/admin.py
+++ houses/admin.py
@@ -4,14 +4,27 @@ from .models import House
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
 
+    #filed 묶기 (price, pets_allowed는 같은 열에 있게 됨)
+    fields = (
+        "name",
+        "address",
+        ("price", "pets_allowed"),
+    )
+    #리스트에서 column으로 보여주기
     list_display = (
         "name",
         "price",
         "address",
         "pets_allowed",
     )
+    #리스트 오른쪽 필터 생성
     list_filter = (
         "price",
         "pets_allowed",
     )
+    #리스트 상단에서 검색 가능
     search_fields = ("address",)
+    #name, address를 누르면 상세 페이지로 이동
+    list_display_links = ("name", "address")
+    #상세 페이지로 이동하지 않아도 pets_allowed 수정 가능
+    list_editable = ("pets_allowed",)
--- houses/models.py
+++ houses/models.py
@@ -5,10 +5,16 @@ class House(models.Model):
     """Model Definition for Houses"""
 
     name = models.CharField(max_length=140)
-    price = models.PositiveIntegerField()
+    price = models.PositiveIntegerField(help_text="Positive Numbers Only")
     description = models.TextField()
     address = models.CharField(max_length=140)
-    pets_allowed = models.BooleanField(default=True)
+    pets_allowed = models.BooleanField(
+        default=True,
+        #도움말
+        help_text="Does this house allow pets?",
+        #기본 title은 변수 이름이지만 verbose_name로 변경 가능
+        verbose_name="Pets Allowed?",
+    )
 
     def __str__(self):
         return self.name
