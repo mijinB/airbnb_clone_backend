8d15f90ac61bb861d153b6728884e039c75019d8
mijinB <bmj13465@gmail.com>
2024-06-19 23:07:26 +0900

    user model custom

--- users/models.py
+++ users/models.py
@@ -5,4 +5,8 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     """Model Definition for Users"""
 
-    pass
+    first_name = models.CharField(max_length=150, editable=False)
+    last_name = models.CharField(max_length=150, editable=False)
+    name = models.CharField(max_length=150, default="")
+    # BooleanField는 non-nullable field라서 default 값을 지정해주거나 null=True로 지정해줘야 migration 가능
+    is_host = models.BooleanField(default=False)
