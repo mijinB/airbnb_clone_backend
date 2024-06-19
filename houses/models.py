d14b771a0595ea4bde509f02cf70ac6213608d8d
mijinB <bmj13465@gmail.com>
2024-06-19 21:51:29 +0900

    black formatter 세팅 및 경고 줄 무시 주석 추가(poetry 선택해도 해결이 안돼서 추가함)

--- houses/models.py
+++ houses/models.py
@@ -1,7 +1,7 @@
-from django.db import models
+from django.db import models  # type: ignore
+
 
 class House(models.Model):
-
     """Model Definition for Houses"""
 
     name = models.CharField(max_length=140)
@@ -10,9 +10,9 @@ class House(models.Model):
     address = models.CharField(max_length=140)
     pets_allowed = models.BooleanField(
         default=True,
-        #도움말
+        # 도움말
         help_text="Does this house allow pets?",
-        #기본 title은 변수 이름이지만 verbose_name로 변경 가능
+        # 기본 title은 변수 이름이지만 verbose_name로 변경 가능
         verbose_name="Pets Allowed?",
     )
 
