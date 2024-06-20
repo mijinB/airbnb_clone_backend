0a7263f5d44f767379ac10bb73a476e71f70d89b
mijinB <bmj13465@gmail.com>
2024-06-20 22:48:18 +0900

    user와 house 연결(house 주인 설정)

--- houses/models.py
+++ houses/models.py
@@ -15,6 +15,10 @@ class House(models.Model):
         # 기본 title은 변수 이름이지만 verbose_name로 변경 가능
         verbose_name="Pets Allowed?",
     )
+    # 사용자가 계정을 지워도 house는 주인이 없는 상태로 남게된다.
+    # owner = models.ForeignKey("users.User", on_delete=models.SET_NULL)
+    # 사용자가 계정을 지우면 house도 함께 지워진다.
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
