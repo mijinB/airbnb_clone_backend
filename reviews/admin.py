b7b7140b4aed0151b1f5a95cbb0810499b554498
Bmijin <bmj13465@jtts.co.kr>
2024-07-02 16:59:00 +0900

    12분 43초

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,6 +2,24 @@ from django.contrib import admin
 from .models import Review
 
 
+class WordFilter(admin.SimpleListFilter):
+
+    title = "Filter by words!"
+
+    parameter_name = "word"  # lookups에서 선택 시, url param으로 보이는데 해당 url param의 name이 "word"로 설정된다. (?word=good)
+
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good"),
+            ("great", "Great"),
+            ("awesome", "Awesome"),
+        ]
+
+    def queryset(self, request, reviews):
+        print(request.GET)
+        return reviews
+
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
 
@@ -9,4 +27,10 @@ class ReviewAdmin(admin.ModelAdmin):
         "__str__",
         "payload",
     )
-    list_filter = ("rating",)
+    list_filter = (
+        WordFilter,
+        "rating",
+        "user__is_host",
+        "room__category",
+        "room__pet_friendly",
+    )
