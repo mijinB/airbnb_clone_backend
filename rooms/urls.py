04b3d9931ef16b45c48cd801df223f8ed70af4c1
Bmijin <bmj13465@jtts.co.kr>
2024-07-03 10:17:49 +0900

    try except문 (try catch문)

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.see_all_rooms),
     # <받을 파라미터의 type:받은 파라미터를 부를 별명>
-    path("<int:room_id>", views.see_one_room),
+    path("<int:room_pk>", views.see_one_room),
 ]
