bb0bc3c3a6e3f539676571c9cddfb32f1f6cb214
Bmijin <bmj13465@jtts.co.kr>
2024-07-03 09:46:50 +0900

    url param 받기

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.say_hello),
+    path("", views.see_all_rooms),
+    # <받을 파라미터의 type:받은 파라미터를 부를 별명>
+    path("<int:room_id>", views.see_one_room),
 ]
