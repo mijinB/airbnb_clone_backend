c2a07033b466159d219b7d84197f4357195cf430
Bmijin <bmj13465@jtts.co.kr>
2024-07-04 08:56:43 +0900

    django rest framework 설치 및 세팅

--- config/settings.py
+++ config/settings.py
@@ -52,7 +52,11 @@ CUSTOM_APPS = [
     "direct_messages.apps.DirectMessagesConfig",
 ]
 
-INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS
+THIRD_PARTY_APPS = [
+    "rest_framework",
+]
+
+INSTALLED_APPS = SYSTEM_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
