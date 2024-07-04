dc74303588bfbdc85193ad9cbc4cb88ea63385a8
Bmijin <bmj13465@jtts.co.kr>
2024-07-04 09:25:41 +0900

    Django serializers framework 사용해서 rest api 만드는 법

--- categories/views.py
+++ categories/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.core import serializers
 from .models import Category
 
 
-# Django REST framework 없이 만드는 방법
+# Django serialization framework 사용해서 만드는 방법
+# custom을 위한 많은 기능을 제공하지 않는다.
 
 
 def categories(request):
@@ -11,6 +13,6 @@ def categories(request):
     return JsonResponse(
         {
             "ok": True,
-            "categories": all_categories,
+            "categories": serializers.serialize("json", all_categories),
         },
     )
