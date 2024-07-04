efc707d42c5c88c035af2cace87661bdd29d017a
Bmijin <bmj13465@jtts.co.kr>
2024-07-04 09:19:32 +0900

    Django REST framework 없이 rest api 만드는 법

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Category
 
-# Create your views here.
+
+# Django REST framework 없이 만드는 방법
+
+
+def categories(request):
+    all_categories = Category.objects.all()
+    return JsonResponse(
+        {
+            "ok": True,
+            "categories": all_categories,
+        },
+    )
