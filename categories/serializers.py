3a3bedcba3986e516a2cdd5d658569c901cf7c53
Bmijin <bmj13465@jtts.co.kr>
2024-07-04 11:19:42 +0900

    PUT (update) 메서드 추가

--- categories/serializers.py
+++ categories/serializers.py
@@ -20,3 +20,10 @@ class CategorySerializer(serializers.Serializer):
 
     def create(self, validated_data):
         return Category.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        # .get ⇒ 첫번째 인자가 없을 경우에 두번째 인자를 default 값으로 설정 (=사용자가 name을 보내지 않았다면 현재의 값을 보내서 사용한다.)
+        instance.name = validated_data.get("name", instance.name)
+        instance.kind = validated_data.get("kind", instance.kind)
+        instance.save()
+        return instance
--- categories/views.py
+++ categories/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from .models import Category
 from .serializers import CategorySerializer
@@ -23,7 +24,7 @@ def categories(request):
     elif request.method == "POST":
         serializer = CategorySerializer(data=request.data)
         if serializer.is_valid():
-            # save()를 실행하면, serializer은 자동으로 create 메서드를 찾기 시작한다.
+            # 사용자한테서 받은 data 밖에 없으므로 create 메서드 실행
             new_category = serializer.save()
             return Response(
                 CategorySerializer(new_category).data,
@@ -32,8 +33,29 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view()
+@api_view(["GET", "PUT"])
 def category(request, pk):
-    category = Category.objects.get(pk=pk)
-    serializer = CategorySerializer(category)
-    return Response(serializer.data)
+    # 사용자가 요청한 (pk가 일치하는)category가 없을경우, 예외처리
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        raise NotFound
+
+    if request.method == "GET":
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = CategorySerializer(
+            category,
+            data=request.data,
+            # 들어오는 data가 완벽한 형태가 아닐 수도 있다는 것을 알려줌 (=사용자한테서 받지 못한 data는 현재 data로 유지해야 한다는 것)
+            partial=True,
+        )
+        if serializer.is_valid():
+            # DB에서 가져온 data와 사용자한테서 받은 data로 serializer를 만든다는 것을 알기 때문에 create 메서드를 실행하지않고 update 메서드를 실행한다.
+            updated_category = serializer.save()
+            return Response(
+                CategorySerializer(updated_category).data,
+            )
+        else:
+            return Response(serializer.errors)
