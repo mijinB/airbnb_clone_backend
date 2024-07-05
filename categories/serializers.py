bf7c1b16b86418edf777cc4e982f8885f3a705c1
Bmijin <bmj13465@jtts.co.kr>
2024-07-05 09:16:05 +0900

    ModelSerializer 사용

--- categories/serializers.py
+++ categories/serializers.py
@@ -2,27 +2,15 @@ from rest_framework import serializers
 from .models import Category
 
 
-class CategorySerializer(serializers.Serializer):
-
-    pk = serializers.IntegerField(
-        read_only=True,
-    )
-    name = serializers.CharField(
-        required=True,
-        max_length=50,
-    )
-    kind = serializers.ChoiceField(
-        choices=Category.CategoryKindChoices.choices,
-    )
-    created_at = serializers.DateTimeField(
-        read_only=True,
-    )
-
-    def create(self, validated_data):
-        return Category.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.kind = validated_data.get("kind", instance.kind)
-        instance.save()
-        return instance
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+        """ 포함시킬 속성 설정
+        fields = (
+            "name",
+            "kind",
+        )
+        fields = "__all__" """
+        """ 제외시킬 속성 설정
+        exclude = ("created_at",) """
