69b1a910720a6ef8532b0889a44cbce4b715761d
mijinB <bmj13465@gmail.com>
2024-07-06 20:13:18 +0900

    관계성 확장 depth

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,5 +1,13 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Amenity
+from .models import Room, Amenity
+
+
+class RoomSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = "__all__"
+        # 관계성(relationship)을 확장하는 첫번째 방법. 하지만, custom이 불가능하다. 모든 정보가 다 보인다.
+        depth = 1
 
 
 class AmenitySerializer(ModelSerializer):
