ae45c1432c488084b36eb1363135155cff63c7c7
mijinB <bmj13465@gmail.com>
2024-07-07 20:35:09 +0900

    wishlist toggle 기능 api (put) 생성

--- wishlists/views.py
+++ wishlists/views.py
@@ -5,6 +5,7 @@ from rest_framework.status import HTTP_200_OK
 from rest_framework.exceptions import NotFound
 from .models import Wishlist
 from .serializers import WishlistSerializer
+from rooms.models import Room
 
 
 class Wishlists(APIView):
@@ -31,7 +32,7 @@ class Wishlists(APIView):
             return Response(serializer.errors)
 
 
-class WishListDetail(APIView):
+class WishlistDetail(APIView):
     permission_classes = [IsAuthenticated]
 
     def get_object(self, pk, user):
@@ -42,7 +43,10 @@ class WishListDetail(APIView):
 
     def get(self, request, pk):
         wishlist = self.get_object(pk, request.user)
-        serializer = WishlistSerializer(wishlist)
+        serializer = WishlistSerializer(
+            wishlist,
+            context={"request": request},
+        )
         return Response(serializer.data)
 
     def put(self, request, pk):
@@ -62,4 +66,27 @@ class WishListDetail(APIView):
     def delete(self, request, pk):
         wishlist = self.get_object(pk, request.user)
         wishlist.delete()
+        return Response(status=HTTP_200_OK)
+
+
+class WishlistToggle(APIView):
+    def get_list(self, pk, user):
+        try:
+            return Wishlist.objects.get(pk=pk, user=user)
+        except Wishlist.DoesNotExist:
+            raise NotFound
+
+    def get_room(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
+    def put(self, request, pk, room_pk):
+        wishlist = self.get_list(pk, request.user)
+        room = self.get_room(room_pk)
+        if wishlist.rooms.filter(pk=room.pk).exists():
+            wishlist.rooms.remove(room)
+        else:
+            wishlist.rooms.add(room)
         return Response(status=HTTP_200_OK)
