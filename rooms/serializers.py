3607dd2bea0ed4315a407aab55231e94ee3a4bc1
mijinB <bmj13465@gmail.com>
2024-07-07 11:45:07 +0900

    custom field (function) 추가

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,10 +1,10 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Room, Amenity
 from users.serializers import TinyUserSerializer
 from categories.serializers import CategorySerializer
 
 
-class AmenitySerializer(ModelSerializer):
+class AmenitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Amenity
         fields = (
@@ -13,7 +13,10 @@ class AmenitySerializer(ModelSerializer):
         )
 
 
-class RoomListSerializer(ModelSerializer):
+class RoomListSerializer(serializers.ModelSerializer):
+
+    rating = serializers.SerializerMethodField()
+
     class Meta:
         model = Room
         fields = (
@@ -22,15 +25,25 @@ class RoomListSerializer(ModelSerializer):
             "country",
             "city",
             "price",
+            "rating",
         )
 
+    def get_rating(self, room):
+        return room.rating()
 
-class RoomDetailSerializer(ModelSerializer):
+
+class RoomDetailSerializer(serializers.ModelSerializer):
 
     owner = TinyUserSerializer(read_only=True)
     amenities = AmenitySerializer(read_only=True, many=True)
     category = CategorySerializer(read_only=True)
+    # SerializerMethodField : field의 값을 계산할 method를 만들으라는 것
+    rating = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
         fields = "__all__"
+
+    # method 이름은 field 이름 앞에 get_을 붙여줘야 한다.
+    def get_rating(self, room):
+        return room.rating()
