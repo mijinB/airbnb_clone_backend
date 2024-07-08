28ea2c27b81908d1fec408082deeb2166fc2edda
Bmijin <bmj13465@jtts.co.kr>
2024-07-08 10:03:53 +0900

    특정 field validation custom

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -1,5 +1,33 @@
+from django.utils import timezone
 from rest_framework import serializers
 from .models import Booking
+
+
+class CreateRoomBookingSerializer(serializers.ModelSerializer):
+    # (null=True, blank=True,) null을 허용하는 field로 만들었기 때문에 null이 되지 않도록 덮어씌운 것이다.
+    check_in = serializers.DateField()
+    check_out = serializers.DateField()
+
+    class Meta:
+        model = Booking
+        fields = (
+            "check_in",
+            "check_out",
+            "guests",
+        )
+
+    # 특정 field의 validation을 customize하는 방법
+    def validate_check_in(self, value):
+        now = timezone.localtime(timezone.now()).date()
+        if now > value:
+            raise serializers.ValidationError("Can't book in the past!")
+        return value
+
+    def validate_check_out(self, value):
+        now = timezone.localtime(timezone.now()).date()
+        if now > value:
+            raise serializers.ValidationError("Can't book in the past!")
+        return value
 
 
 class PublicBookingSerializer(serializers.ModelSerializer):
