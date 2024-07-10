5aaab56215903c7db8029626bc2c043c632fafce
Bmijin <bmj13465@jtts.co.kr>
2024-07-10 09:20:39 +0900

    users (post) /create user

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.exceptions import ParseError
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from . import serializers
 
@@ -22,6 +23,23 @@ class Me(APIView):
         )
         if serializer.is_valid():
             user = serializer.save()
+            serializer = serializers.PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class Users(APIView):
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise ParseError
+        serializer = serializers.PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            # raw password를 저장하지 않고, password를 hash화해서 저장할 건데 set_password(raw password)를 작성하면 django가 해줌
+            user.set_password(password)
+            user.save()
             serializer = serializers.PrivateUserSerializer(user)
             return Response(serializer.data)
         else:
