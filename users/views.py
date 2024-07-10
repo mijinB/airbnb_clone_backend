068fa5d485b88d4d7251cf3d318c1ea0b090ae73
Bmijin <bmj13465@jtts.co.kr>
2024-07-10 09:56:30 +0900

    user/log-in (post)

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -67,9 +68,26 @@ class ChangePassword(APIView):
         if not old_password or not new_password:
             raise ParseError
         if user.check_password(old_password):
-            # set_password는 hash값만 적용된다. save()는 따로 꼭 해주어야 된다.
             user.set_password(new_password)
             user.save()
             return Response(status=status.HTTP_200_OK)
         else:
             return ParseError
+
+
+class Login(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            raise ParseError
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+        if user:
+            login(request, user)
+            return Response({"ok": "Welcome!"})
+        else:
+            return Response({"error": "wrong password"})
