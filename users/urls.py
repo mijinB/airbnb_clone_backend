d00541a58a2bc5c270bd295037c19c8924ce03bb
mijinB <bmj13465@gmail.com>
2024-07-12 16:26:30 +0900

    custom authentication2 (pyjwt)

--- users/urls.py
+++ users/urls.py
@@ -6,8 +6,12 @@ urlpatterns = [
     path("", views.Users.as_view()),
     path("me", views.Me.as_view()),
     path("change-password", views.ChangePassword.as_view()),
+    # username & password 이용한 login
     path("log-in", views.Login.as_view()),
     path("log-out", views.Logout.as_view()),
+    # username & password & auth Token 이용한 login
     path("token-login", obtain_auth_token),
+    # username & password 이용한 JWT login
+    path("jwt-login", views.JWTLogin.as_view()),
     path("@<str:username>", views.PublicUser.as_view()),
 ]
