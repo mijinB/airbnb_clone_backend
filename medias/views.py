677f5af4aaf1d25a1d3bdfafe5295bd29d1ac5b2
mijinB <bmj13465@gmail.com>
2024-09-12 22:41:10 +0900

    upload한 image url 가져오기

--- medias/views.py
+++ medias/views.py
@@ -1,3 +1,5 @@
+import requests
+from django.conf import settings
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -23,3 +25,14 @@ class PhotoDetail(APIView):
             raise PermissionDenied
         photo.delete()
         return Response(status=HTTP_200_OK)
+
+
+class GetUploadURL(APIView):
+    def post(self, request):
+        url = f"https://api.cloudflare.com/client/v4/accounts/{settings.CF_ID}/images/v2/direct_upload"
+        one_time_url = requests.post(
+            url, heaaders={"Authorization": f"Bearer {settings.CF_TOKEN}"}
+        )
+        one_time_url = one_time_url.json()
+        result = one_time_url.get("result")
+        return Response({"uploadURL": result.get("uploadURL")})
